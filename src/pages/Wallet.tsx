9e806de57e1cf484f865d73a97045644395f2b61
Arnav-Nehra <arnavnehra1@gmail.com>
2024-10-16 21:11:56 +0530

    Revert "frontend"
    This reverts commit 65aaf869379793d3c16b9f29a637e94fd6986114.
    
    reverted

--- src/pages/Wallet.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from "../components/Header"
-import { SelectWallet } from "../components/SelectWallet"
-
-export const Wallet = ():JSX.Element =>{
-    return <div  className="bg-customBeige min-h-screen  dark:bg-black"> 
-        <Header></Header>
-        <SelectWallet></SelectWallet>
-    </div>
-}
